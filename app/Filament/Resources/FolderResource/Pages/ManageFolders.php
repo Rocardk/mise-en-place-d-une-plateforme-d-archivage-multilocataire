fde3ee6f5b351ca804781077d5631d66c6e98786
Rocardk <rkamtchi@gmail.com>
2024-02-23 14:31:42 +0100

    Adding folder management

---
+++ app/Filament/Resources/FolderResource/Pages/ManageFolders.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\FolderResource\Pages;
+
+use App\Filament\Resources\FolderResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageFolders extends ManageRecords
+{
+    protected static string $resource = FolderResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
